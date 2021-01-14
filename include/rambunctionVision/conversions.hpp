ad688dbfb194e0fafecb6b1665d5308048d40bf2
gcjurgiel <gcjurgiel@icloud.com>
2021-01-14 16:16:36 -0600

    Moved implementation to header to avoid linker error

--- include/rambunctionVision/conversions.hpp
+++ include/rambunctionVision/conversions.hpp
@@ -6,16 +6,36 @@
 
 namespace rv {
   template<typename _OutType, typename _InType>
-  std::vector<cv::Point_<_OutType>> convertToPoints(std::vector<cv::Point_<_InType>>& input);
+  std::vector<cv::Point_<_OutType>> convertToPoints(std::vector<cv::Point_<_InType>>& input) {
+    std::vector<cv::Point_<_OutType>> output;
+    std::transform(input.begin(), input.end(), output.begin(), [](const cv::Point_<_InType>& p) {
+      return cv::Point_<_OutType>(static_cast<_OutType>(p.x), static_cast<_OutType>(p.y));
+    });
+  }
 
   template<typename _OutType, typename _InType>
-  std::vector<cv::Point_<_OutType>> convertToPoints(std::vector<cv::Point3_<_InType>>& input);
+  std::vector<cv::Point_<_OutType>> convertToPoints(std::vector<cv::Point3_<_InType>>& input) {
+    std::vector<cv::Point_<_OutType>> output;
+    std::transform(input.begin(), input.end(), output.begin(), [](const cv::Point3_<_InType>& p) {
+      return cv::Point_<_OutType>(static_cast<_OutType>(p.x), static_cast<_OutType>(p.y));
+    });
+  }
 
   template<typename _OutType, typename _InType>
-  std::vector<cv::Point3_<_OutType>> convertToPoints3(std::vector<cv::Point_<_InType>>& input);
+  std::vector<cv::Point3_<_OutType>> convertToPoints3(std::vector<cv::Point_<_InType>>& input) {
+    std::vector<cv::Point3_<_OutType>> output;
+    std::transform(input.begin(), input.end(), output.begin(), [](const cv::Point_<_InType>& p) {
+      return cv::Point3_<_OutType>(static_cast<_OutType>(p.x), static_cast<_OutType>(p.y), 0);
+    });
+  }
 
   template<typename _OutType, typename _InType>
-  std::vector<cv::Point3_<_OutType>> convertToPoints3(std::vector<cv::Point3_<_InType>>& input);
+  std::vector<cv::Point3_<_OutType>> convertToPoints3(std::vector<cv::Point3_<_InType>>& input) {
+    std::vector<cv::Point3_<_OutType>> output;
+    std::transform(input.begin(), input.end(), output.begin(), [](const cv::Point3_<_InType>& p) {
+      return cv::Point3_<_OutType>(static_cast<_OutType>(p.x), static_cast<_OutType>(p.y), static_cast<_OutType>(p.z));
+    });
+  }
 
   template<typename _Type>
   std::vector<_Type> inVector(_Type value) { return std::vector<_Type> {value}; }
