cbfab59d22cde9b9c74bfc474414aa99b9ff8700
gcjurgiel <gcjurgiel@icloud.com>
2021-01-15 17:09:59 -0600

    Readability

--- include/rambunctionVision/imageProcessing.hpp
+++ include/rambunctionVision/imageProcessing.hpp
@@ -14,12 +14,12 @@ namespace rv {
     int& highV() { return high[2]; }
     int& lowV()  { return low[2];  }
 
-    void setHighH(int v) { highH() = std::clamp(v, lowH() + 1, 180); }
-    void setLowH(int v) { lowH() = std::clamp(v, 0, highH() - 1); }
-    void setHighS(int v) { highV() = std::clamp(v, lowS() + 1, 255); }
-    void setLowS(int v) { lowV() = std::clamp(v, 0, highS() - 1); }
-    void setHighV(int v) { highV() = std::clamp(v, lowV() + 1, 255); }
-    void setLowV(int v) { lowV() = std::clamp(v, 0, highV() - 1); }
+    void setHighH(int value) { highH() = std::clamp(value, lowH() + 1, 180); }
+    void setLowH(int value) { lowH() = std::clamp(value, 0, highH() - 1); }
+    void setHighS(int value) { highV() = std::clamp(value, lowS() + 1, 255); }
+    void setLowS(int value) { lowV() = std::clamp(value, 0, highS() - 1); }
+    void setHighV(int value) { highV() = std::clamp(value, lowV() + 1, 255); }
+    void setLowV(int value) { lowV() = std::clamp(value, 0, highV() - 1); }
   };
 
   void thresholdImage(cv::Mat& src, cv::Mat& dst, int blurSize, rv::Threshold threshold, cv::Mat open, cv::Mat close);
