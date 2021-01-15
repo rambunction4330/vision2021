c6e4baf7f85693c1271052a99d0321f7523f871c
gcjurgiel <gcjurgiel@icloud.com>
2021-01-15 17:02:47 -0600

    remove normilization

--- src/rambunctionVision/imageProcessing.cpp
+++ src/rambunctionVision/imageProcessing.cpp
@@ -3,11 +3,10 @@
 #include <opencv2/imgproc.hpp>
 
 namespace rv {
-  void thresholdImage(cv::Mat& src, cv::Mat& dst, int blurSize, rv::Threshold threshold, cv::Mat& openMatrix, cv::Mat& closeMatrix) {
-    cv::Mat norm, blur, hsv, thresh, open, close;
+  void thresholdImage(cv::Mat& src, cv::Mat& dst, int blurSize, rv::Threshold threshold, cv::Mat openMatrix, cv::Mat closeMatrix) {
+    cv::Mat blur, hsv, thresh, open, close;
 
-    cv::normalize(src, norm);
-    cv::blur(norm, blur, cv::Size(blurSize, blurSize));
+    cv::blur(src, blur, cv::Size(blurSize, blurSize));
 
     cv::cvtColor(blur, hsv, cv::COLOR_BGR2HSV);
     cv::inRange(hsv, cv::Scalar(threshold.low), cv::Scalar(threshold.high), thresh);
