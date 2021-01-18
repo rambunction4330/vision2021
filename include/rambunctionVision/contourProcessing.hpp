508637df2d078ed0dc735e810b7aed8ee0d94f9b
gcjurgiel <gcjurgiel@icloud.com>
2021-01-18 11:15:32 -0600

    writing to a file

--- include/rambunctionVision/contourProcessing.hpp
+++ include/rambunctionVision/contourProcessing.hpp
@@ -9,7 +9,28 @@ namespace rv {
   struct Target {
     std::string name;
     std::vector<cv::Point2f> shape;
+
+    void write(cv::FileStorage& fs) const {
+      fs << "{" << "Name" << name << "Shape" << shape << "}";
+    }
+
+    void read(const cv::FileNode& node) {
+      node["Name"] >> name;
+      node["Shape"] >> shape;
+    }
   };
+
+  static void write(cv::FileStorage& fs, const std::string&, const rv::Target& x) { 
+    x.write(fs);
+  }
+  
+  static void read(const cv::FileNode& node, rv::Target& x, const rv::Target& default_value = rv::Target()){
+    if(node.empty()) {
+      x = default_value;
+    } else {
+      x.read(node);
+    }
+  }
 
   struct TargetMatch {
     std::vector<cv::Point2f> shape;
@@ -28,7 +49,28 @@ namespace rv {
     cv::Point3f center;
 
     std::vector<cv::Point3f> points();
+
+    void write(cv::FileStorage& fs) const {
+      fs << "{" << "Radius" << radius << "Center" << center << "}";
+    }
+
+    void read(const cv::FileNode& node) {
+      node["Radius"] >> radius;
+      node["Center"] >> center;
+    }
   };
+
+  static void write(cv::FileStorage& fs, const std::string&, const rv::Ball& x) { 
+    x.write(fs);
+  }
+  
+  static void read(const cv::FileNode& node, rv::Ball& x, const rv::Ball& default_value = rv::Ball()){
+    if(node.empty()) {
+      x = default_value;
+    } else {
+      x.read(node);
+    }
+  }
 
   struct Circle {
     float radius; 
