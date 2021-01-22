d3ead5fdee3fa56f01a35b5e76942191af2948b7
gcjurgiel <gcjurgiel@icloud.com>
2021-01-22 16:04:28 -0600

    Reformatting and coments

--- src/tools/targetBuilder/main.cpp
+++ src/tools/targetBuilder/main.cpp
@@ -5,6 +5,7 @@
 #include <rambunctionVision/contourProcessing.hpp>
 
 int main (int argc, char** argv) {
+
   // Keys for argument parsing
   const std::string keys = 
   "{ h ? help usage | | prints this message             }"
@@ -31,44 +32,57 @@ int main (int argc, char** argv) {
     std::cout << "\nEnter Ball radius: ";
     std::cin >> ball.radius;
 
+    // It is easier to parse formated data with the C function as oposed to cin,
+    // but to get it to work reliably, I found it is best to pull it to a string
+    // and then parse the string rather than parsing the raw input.
     std::cout << "Enter Ball center: ";
     std::string input;
     std::cin >> input;
-    if (sscanf(input.c_str(), "(%f,%f,%f)", &ball.center.x, &ball.center.y, &ball.center.z) != 3) {
+    int numParsed = sscanf(input.c_str(), "(%f,%f,%f)", &ball.center.x, &ball.center.y, &ball.center.z);
+    
+    if (numParsed != 3) {
       ball.center = {0,0,0};
     }
 
     std::cout << "\n";
 
+    // Once completed, Write the ball to a file
     if (outputFile != "") {
       cv::FileStorage storage(outputFile, cv::FileStorage::WRITE);
       if (storage.isOpened()) {
         storage << "Ball" << ball;
       }
+      storage.release();
     }
 
   } else {
-
     std::vector<rv::Target> targets;
-
     while (true) {
       rv::Target target;
 
       std::cout << "\nEnter the target name: ";
       std::cin >> target.name;
 
+      // TODO: Fix this god auful Interface
       std::cout << "Enter target points (Type 'done!' when finnished):\n\n";
 
       while (true) {
+        // It is easier to parse formated data with the C function as oposed to cin,
+        // but to get it to work reliably, I found it is best to pull it to a string
+        // and then parse the string rather than parsing the raw input.
         std::string input;
         std::cin >> input;
 
+        // Cheack if the user is done
         if (input == "done!" || input == "done") {
           break;
         }
 
         cv::Point2f point;
-        if (sscanf(input.c_str(), "(%f,%f)", &point.x, &point.y) == 2) {
+        int numParsed = sscanf(input.c_str(), "(%f,%f)", &point.x, &point.y);
+
+        // If the parser found both coordinates add it to the shape
+        if (numParsed == 2) {
           target.shape.push_back(point);
         } else {
           std::cout << "Invalid Format! Point will not be added to target.\n";
@@ -76,13 +90,14 @@ int main (int argc, char** argv) {
       }
 
       std::cout << "\n";
-
       targets.push_back(target);
 
+      // Ask the user to add another target
       std::string input;
       std::cout << "Add another Target (yes or no): ";
       std::cin >> input;
 
+      // If they sad yes start adding another, otherwise exit the loop
       if (input == "yes" || input == "y") {
         continue;
       } else {
@@ -90,6 +105,7 @@ int main (int argc, char** argv) {
       }
     }
 
+    // Write the targets to a file
     if (outputFile != "") {
       cv::FileStorage storage(outputFile, cv::FileStorage::WRITE);
       if (storage.isOpened()) {
